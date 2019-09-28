ba334f86fdfe705dc4c4208f06f387ccf0c598d9
GSM Rana <gsmrana@gmail.com>
2019-09-28 22:44:22 +0600

    Initial design

---
+++ Base64 Encoder Decoder/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Base64_Encoder_Decoder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly MainWindowViewModel _viewModel = new MainWindowViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = _viewModel;
+            _viewModel.EncodedText = "VGhpcyBpcyBhIFRlc3Qh";
+            _viewModel.DecodedText = "This is a Test!";
+        }
+
+        void PopupException(string message, string caption = "Exception")
+        {
+            MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void ButtonEncode_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var inputstr = _viewModel.DecodedText;
+                var inputbytes = new byte[0];
+                if (inputstr.StartsWith("0x")) //from hex string
+                {
+                    inputstr = inputstr.Substring(2);
+                    inputstr = inputstr.Replace("-", "");
+                    inputbytes = Enumerable.Range(0, inputstr.Length)
+                                           .Where(x => x % 2 == 0)
+                                           .Select(x => Convert.ToByte(inputstr.Substring(x, 2), 16))
+                                           .ToArray();
+                }
+                else  //from ascii string
+                {
+                    inputbytes = Encoding.ASCII.GetBytes(inputstr);
+                }
+                var outputstr = Convert.ToBase64String(inputbytes);
+                _viewModel.EncodedText = outputstr;
+                _viewModel.EncodedTextColor = Colors.Blue.ToString();
+                _viewModel.DecodedTextColor = Colors.Black.ToString();
+            }
+            catch (Exception ex)
+            {
+                PopupException(ex.Message);
+            }
+        }
+
+        private void ButtonDecode_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var inputstr = _viewModel.EncodedText;
+                var inputbytes = Convert.FromBase64String(inputstr);
+                var outputstr = "";
+                if (inputbytes.Any(p => p < 32 || p > 126)) //to hex string
+                {
+                    outputstr = "0x" + BitConverter.ToString(inputbytes);
+                }
+                else  //to ascii string
+                {
+                    outputstr = Encoding.ASCII.GetString(inputbytes);
+                }
+                _viewModel.DecodedText = outputstr;
+                _viewModel.EncodedTextColor = Colors.Black.ToString();
+                _viewModel.DecodedTextColor = Colors.Blue.ToString();
+            }
+            catch (Exception ex)
+            {
+                PopupException(ex.Message);
+            }
+        }
+    }
+}
---
+++ Base64 Encoder Decoder/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base64_Encoder_Decoder
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _encodedText;
+        public string EncodedText
+        {
+            get => _encodedText;
+            set => SetProperty(ref _encodedText, value);
+        }
+
+        private string _encodedTextColor = "Blue";
+        public string EncodedTextColor
+        {
+            get => _encodedTextColor;
+            set => SetProperty(ref _encodedTextColor, value);
+        }
+
+        private string _decodedText;
+        public string DecodedText
+        {
+            get => _decodedText;
+            set => SetProperty(ref _decodedText, value);
+        }
+
+        private string _decodedTextColor = "Black";
+        public string DecodedTextColor
+        {
+            get => _decodedTextColor;
+            set => SetProperty(ref _decodedTextColor, value);
+        }
+
+        protected void SetProperty<T>(ref T field, T newValue, [CallerMemberName]string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
